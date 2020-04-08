2a0405008ac133066e02d7c1387e2991079d41f3
Michał Żołnieruk <mzo@trackman.com>
2020-04-08 12:22:46 +0200

    Simple v3 azure functions project init

---
+++ HttpTrigger1/DummyFunction.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Framework;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
+
+namespace DummyFunctionApp.DummyFunction
+{
+    public class DummyFunction
+    {
+        [FunctionName("DummyFunction")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Admin, "get", Route = "dummyFunction")] HttpRequest req, ILogger log)
+        {
+            return new OkObjectResult("hey!");
+        }
+    }
+}
